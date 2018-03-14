6f31ac010f191c2416b226fe910cc2c9d899adf6
James Rice <james@purplecs.com>
2018-03-14 14:56:09 +0000

    dealing with state & passing functions through props

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,18 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component{
+    state={
+        fishes: {},
+        order: {}
+    };
+    addFish = fish =>{
+        //Copy of existing state
+        const fishes = {...this.state.fishes};
+        //Add fish to the fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        //Update state
+        this.setState({fishes})
+    }
     render(){
         return(
             <div className="catch-of-the-day">
@@ -11,7 +23,7 @@ class App extends React.Component{
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order/>
-                <Inventory/>
+                <Inventory addFish={this.addFish} />
             </div>
         )
     }
