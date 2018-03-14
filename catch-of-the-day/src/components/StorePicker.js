9c67a7c84874a5e2088936769b626b23747caef3
James Rice <james@purplecs.com>
2018-03-14 11:16:44 +0000

    First react components/StorePicker.js

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+
+class StorePicker extends React.Component{
+    render(){
+        return(
+            <Fragment>
+                <form className="store-selector">
+
+                </form>
+            </Fragment>
+        )
+    }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,9 @@
-// let's go!
+import React from 'react';
+//Only need render method
+import {render} from 'react-dom';
+//Import Components
+import StorePicker from './components/StorePicker'
+
+
+//Render your components
+render(<StorePicker/>, document.querySelector('#main'));
